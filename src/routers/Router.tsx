fb3ddcdc159b1e6692cef99d56adeb6c5f7c796e
Thuong <thuongtrinhduc@tsoft.vn>
2026-05-19 18:07:50 +0700

    update router

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -7,7 +7,6 @@ import BookingManagementPage from "@/pages/dashboard/Pages/Page2";
 
 export const WebRouter = () => (
   <Routes>
-    <Route path="/" element={<Navigate to={ROUTER_PATH.DASHBOARD} replace />} />
     <Route path={ROUTER_PATH.LOGIN} element={<Login />} />
     <Route path={ROUTER_PATH.DASHBOARD} element={<AppLayout />}>
       <Route index element={<DashboardPage />} />
